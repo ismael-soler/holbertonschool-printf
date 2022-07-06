2287020d93f244250a87948ce1f08c16fc2ebbf3
ismael-soler <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@github.com>
2022-07-06 09:32:09 -0700

    added get_format.c

---
+++ get_format.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_func - get function according to format
+ * @c: format
+ * Return: pointer to function or NULL
+ */
+
+int (*get_func (char *c))(va_list arg)
+{
+	form_t ff[] = {
+		{'c', form_c},
+		{'s', form_s},
+		{NULL, NULL}
+	};
+
+	int i;
+	for (i = 0; i < 3; i++)
+		if (ff[i].form[0] = c[0])
+			return (ff[i].f);
+	return (NULL);
+}
